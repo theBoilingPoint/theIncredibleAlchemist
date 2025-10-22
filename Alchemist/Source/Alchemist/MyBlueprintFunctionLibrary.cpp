dc9a4df40ab7da2815c9fdd89345156e14c12569
LijunQu <qulijun@seas.upenn.edu>
2025-10-22 19:46:30 -0400

    mixing color for fix amount worked

--- Alchemist/Source/Alchemist/MyBlueprintFunctionLibrary.cpp
+++ Alchemist/Source/Alchemist/MyBlueprintFunctionLibrary.cpp
@@ -21,3 +21,60 @@ FLinearColor UMyBlueprintFunctionLibrary::MixColors(FLinearColor Color1, FLinear
 
     return FLinearColor(outR, outG, outB, 1.0f);
 }
+
+TArray<float> UMyBlueprintFunctionLibrary::RGBToLatent(FLinearColor Color)
+{
+    mixbox_latent latent;
+
+    // Convert RGB to latent space
+    mixbox_linear_float_rgb_to_latent(Color.R, Color.G, Color.B, latent);
+
+    // Convert to TArray for Blueprint
+    TArray<float> result;
+    result.SetNum(MIXBOX_LATENT_SIZE);
+    for (int32 i = 0; i < MIXBOX_LATENT_SIZE; i++)
+    {
+        result[i] = latent[i];
+    }
+
+    return result;
+}
+
+TArray<float> UMyBlueprintFunctionLibrary::MixLatentWithColor(const TArray<float>& CurrentLatent, FLinearColor NewColor, float MixAmount)
+{
+    // Clamp mix amount
+    MixAmount = FMath::Clamp(MixAmount, 0.0f, 1.0f);
+
+    // Convert new color to latent
+    mixbox_latent newLatent;
+    mixbox_linear_float_rgb_to_latent(NewColor.R, NewColor.G, NewColor.B, newLatent);
+
+    // Mix in latent space
+    TArray<float> result;
+    result.SetNum(MIXBOX_LATENT_SIZE);
+
+    for (int32 i = 0; i < MIXBOX_LATENT_SIZE; i++)
+    {
+        // Lerp: (1-t)*current + t*new
+        float currentVal = (CurrentLatent.IsValidIndex(i)) ? CurrentLatent[i] : 0.0f;
+        result[i] = (1.0f - MixAmount) * currentVal + MixAmount * newLatent[i];
+    }
+
+    return result;
+}
+
+FLinearColor UMyBlueprintFunctionLibrary::LatentToRGB(const TArray<float>& Latent)
+{
+    // Copy TArray to mixbox_latent
+    mixbox_latent latent;
+    for (int32 i = 0; i < MIXBOX_LATENT_SIZE; i++)
+    {
+        latent[i] = (Latent.IsValidIndex(i)) ? Latent[i] : 0.0f;
+    }
+
+    // Convert to RGB
+    float r, g, b;
+    mixbox_latent_to_linear_float_rgb(latent, &r, &g, &b);
+
+    return FLinearColor(r, g, b, 1.0f);
+}
--- Alchemist/Source/Alchemist/MyBlueprintFunctionLibrary.h
+++ Alchemist/Source/Alchemist/MyBlueprintFunctionLibrary.h
@@ -18,4 +18,16 @@ public:
 	// Mix two colors using realistic paint mixing
 	UFUNCTION(BlueprintCallable, Category = "Color Mixing")
 	static FLinearColor MixColors(FLinearColor Color1, FLinearColor Color2, float MixAmount);
+
+    // Convert RGB to Latent space (call once when starting or adding new color)
+    UFUNCTION(BlueprintCallable, Category = "Color Mixing")
+    static TArray<float> RGBToLatent(FLinearColor Color);
+
+    // Mix current latent with new color (stores result in latent space)
+    UFUNCTION(BlueprintCallable, Category = "Color Mixing")
+    static TArray<float> MixLatentWithColor(const TArray<float>& CurrentLatent, FLinearColor NewColor, float MixAmount);
+
+    // Convert Latent back to RGB for display
+    UFUNCTION(BlueprintCallable, Category = "Color Mixing")
+    static FLinearColor LatentToRGB(const TArray<float>& Latent);
 };
